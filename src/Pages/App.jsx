9ecd72787adbbbb7365158702a81803e41f6d978
Tony Migeon <rhyyn.lol@gmail.com>
2022-09-07 10:52:02 +0200

    Background img styling + added Header

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import styles from '../../styles/globals.module.scss'
+import { useState } from "react";
+import styles from "../../styles/globals.module.scss";
+import Header from "../Components/Header";
 
 function App() {
-
-  return (
-    <div className="App">
-      <section className = {styles.mainSection}>
-        <div className = {styles.backgroundImageContainer}>
-        <div className = {styles.image}></div>
+    return (
+        <div className="App">
+            <section className={styles.mainSection}>
+                <div className={styles.backgroundImageContainer}>
+                    <div className={styles.image}>
+                      <Header></Header>
+                    </div>
+                </div>
+                <div className={styles.backgroundWhite}></div>
+            </section>
         </div>
-        <div className = {styles.backgroundWhite}></div>
-      </section>
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
