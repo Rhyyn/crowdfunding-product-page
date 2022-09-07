c0cd616bc5c31ff53907c3cba76377f31b03f358
Tony Migeon <rhyyn.lol@gmail.com>
2022-09-07 15:12:01 +0200

    Moved Header, added linear gradient

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import styles from "../../styles/globals.module.scss";
 import Header from "../Components/Header";
+import Content from "../Components/Content/Content";
 
 function App() {
     return (
         <div className="App">
             <section className={styles.mainSection}>
+                <Header></Header>
                 <div className={styles.backgroundImageContainer}>
-                    <div className={styles.image}>
-                      <Header></Header>
-                    </div>
+                    <div className={styles.image}></div>
                 </div>
-                <div className={styles.backgroundWhite}></div>
+                <div className={styles.backgroundWhite}>
+                    <Content></Content>
+                </div>
             </section>
         </div>
     );
