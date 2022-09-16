b6b0317327ea5c63b577e21b7769f3264bc274da
Tony Migeon <rhyyn.lol@gmail.com>
2022-09-16 15:39:50 +0200

    Start of About Component

---
+++ src/Components/Content/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../../styles/Content/About.module.scss";
+
+const About = () => {
+    return <div className = {styles.aboutContainer}>About</div>;
+};
+
+export default About;
--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "../../../styles/Content/Content.module.scss";
 import Intro from "./Intro";
 import Status from "./Status";
+import About from "./About";
 
 const Content = () => {
     return (
         <section className={styles.contentSection}>
             <Intro></Intro>
             <Status></Status>
+            <About></About>
         </section>
     );
 };
