f5eed32f14f8482b9897ff7e51f5a0c150f807c7
Tony Migeon <rhyyn.lol@gmail.com>
2022-09-19 16:38:50 +0200

    About + layout tablet

--- src/Components/Content/About.jsx
+++ src/Components/Content/About.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "../../../styles/Content/About.module.scss";
 
 const About = () => {
-    return <div className = {styles.aboutContainer}>About</div>;
+    return (
+        <div className={styles.aboutContainer}>
+            <h1 className = {styles.title}>About this project</h1>
+            <span className = {styles.desc}>
+                The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
+                platform that elevates your screen to a more comfortable viewing
+                height. Placing your monitor at eye level has the potential to
+                improve your posture and make you more comfortable while at
+                work, helping you stay focused on the task at hand. Featuring
+                artisan craftsmanship, the simplicity of design creates extra
+                desk space below your computer to allow notepads, pens, and USB
+                sticks to be stored under the stand.
+            </span>
+        </div>
+    );
 };
 
 export default About;
